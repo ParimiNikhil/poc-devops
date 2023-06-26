9bba60f2e34af7e4f26d6c41ffadaa3022bbb8f4
pariminikhil <pariminikhil1996@gmail.com>
2023-06-26 10:09:22 +0530

    saving maven files

---
+++ src/main/java/com/poc/devops/PylifeApplication.java
@@ -0,0 +1,13 @@
+package com.poc.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PylifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PylifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/devops/PylifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PylifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
